cbeb281c83bde012a3cc545d2cf638ba989a08e3
zenab mohammed abdelmonaem <78083890+zenab12@users.noreply.github.com>
2022-02-04 12:30:58 +0200

    Add files via upload

--- colorPicker.js
+++ colorPicker.js
@@ -1,43 +1,47 @@
+let sizePicker = document.getElementById('sizePicker');
+let color = document.getElementById("colorPicker");
+let gridHeight = document.getElementById('inputHeight');
+let gridWidth = document.getElementById('inputWidth');
+let canvas = document.getElementById("pixelCanvas");
+let Reset = document.querySelector('#Reset')
+let Pallette = document.querySelector('.pallette');
+let name = document.querySelector('.pallette h2');
+let PalletteName = prompt('Enter Your Pallette Name : ');
 
-var sizePicker = document.getElementById('sizePicker');
-var color = document.getElementById("colorPicker");
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+name.innerHTML = PalletteName
+name.style.display = 'block';
 
 function makeGrid() {
-    
-var gridHeight = document.getElementById('inputHeight');
-var gridWidth = document.getElementById('inputWidth');
 
- var canvas = document.getElementById("pixelCanvas");
- while (canvas.firstChild) {
-    canvas.removeChild(canvas.firstChild);
+    for (let i = 0; i < parseInt(gridHeight.value); i++) {
+        let tr = document.createElement('tr');
+        canvas.appendChild(tr);
+
+        for (j = 0; j < parseInt(gridWidth.value); j++) {
+            let td = document.createElement('td');
+            tr.appendChild(td);
+            td.style.height = 6;
+            td.style.width = 20;
+            td.style.backgroundColor = color.value;
+            td.onclick = function() { this.style.backgroundColor = color.value }
+        }
+
     }
 
-for (var i = 0 ; i < gridHeight.value; i = i + 1) {
-    var tr = document.createElement('tr');
-    canvas.appendChild(tr); 
-       /*for(j =1; j <= i; j = j +1){
-         var td = document.createElement('td');
-           tr.appendChild(td);
-       }*/
-    for (j = 0; j < gridWidth.value; j = j +1){
-   var td = document.createElement('td');
-    tr.appendChild(td);
-    td.style.height = 6;
-    td.style.width = 20;
-    td.style.backgroundColor = color.value;
-    td.onclick = function() {this.style.backgroundColor = color.value }
-}
 }
 
+function ResetTheSize() {
+    //Reset The size 
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+    gridHeight.value = 0
+    gridWidth.value = 0
 }
 
-var submit = document.getElementById('submit');
+Reset.addEventListener('click', ResetTheSize);
 
 sizePicker.addEventListener('submit', function(e) {
     e.preventDefault();
     makeGrid();
-  });
-
+});
